b9315277bb7212b59ea02ad237ffcf4a09b1ca1d
Zhenggang Su <zgsu@alauda.io>
2020-12-02 17:42:48 +0800

    feat: add gojs-angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GojsAngularModule } from "gojs-angular";
+
+import { InspectorComponent } from "./inspector/component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InspectorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    GojsAngularModule
   ],
   providers: [],
   bootstrap: [AppComponent]
